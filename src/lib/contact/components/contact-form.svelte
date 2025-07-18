4be8696261d32d29fa4199105576f95326a965f1
Paresh Kumar Pradhan <pareshkupradhan789@gmail.com>
2025-07-18 11:28:09 +0530

    Linked formspree

--- src/lib/contact/components/contact-form.svelte
+++ src/lib/contact/components/contact-form.svelte
@@ -1,79 +1,81 @@
 <script lang="ts">
-   import {
-       Button,
-       EmailField,
-       InputField,
-       PhoneField,
-       showToast,
-       TextareaField
-   } from '@cloudparker/moldex.js';
+	import {
+		Button,
+		EmailField,
+		InputField,
+		PhoneField,
+		showToast,
+		TextareaField
+	} from '@cloudparker/moldex.js';
 
-   let name: string = $state('');
-   let email: string = $state('');
-   let phone: string = $state('');
-   let message: string = $state('');
+	let name: string = $state('');
+	let email: string = $state('');
+	let phone: string = $state('');
+	let message: string = $state('');
 
-	async function handleSubmit(e:any) {
-    e.preventDefault();
+	async function handleSubmit(e: any) {
+		e.preventDefault();
 
-    // Trim inputs
-    name = (name || '').trim();
-    email = (email || '').trim();
-    phone = (phone || '').trim();
-    message = (message || '').trim();
+		// Trim inputs
+		name = (name || '').trim();
+		email = (email || '').trim();
+		// phone = (phone || '').trim();
+		// message = (message || '').trim();
 
-    // Validate required fields
-    if (name && message && (email || phone)) {
-      try {
-        const response = await fetch('https://formspree.io/f/xldjdjyw', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            name,
-            email,
-            phone,
-            message
-          })
-        });
+		// Validate required fields
+		if (name && email) {
+			try {
+				const response = await fetch('https://formspree.io/f/xzzvzgyd', {
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json'
+					},
+					body: JSON.stringify({
+						name,
+						email
+					})
+				});
 
-        if (response.ok) {
-          showToast({ msg: 'Message sent successfully!' });
+				if (response.ok) {
+					showToast({ msg: 'Message sent successfully!' });
 
-          // Reset form fields
-          name = '';
-          email = '';
-          phone = '';
-          message = '';
-        } else {
-          showToast({ msg: 'Failed to send message. Please try again later.' });
-        }
-      } catch (error) {
-        console.error('Error submitting form:', error);
-        showToast({ msg: 'Something went wrong!' });
-      }
-    } else {
-      showToast({ msg: 'Missing Required Fields!' });
-    }
-  }
+					// Reset form fields
+					name = '';
+					email = '';
+				} else {
+					showToast({ msg: 'Failed to send message. Please try again later.' });
+				}
+			} catch (error) {
+				console.error('Error submitting form:', error);
+				showToast({ msg: 'Something went wrong!' });
+			}
+		} else {
+			showToast({ msg: 'Missing Required Fields!' });
+		}
+	}
 </script>
 
 <div class="relative mx-auto my-16 rounded-lg p-0.5 px-4 shadow-xl md:my-32 md:px-16 lg:px-32">
-	<div class="bg-base-900 relative z-10 rounded-lg p-6 md:p-8">
+	<div class="relative z-10 rounded-lg bg-base-900 p-6 md:p-8">
 		<h2 class="mb-4 text-center text-2xl font-bold text-white md:text-3xl">Get in Touch</h2>
-		<p class="text-base-400 mb-8 text-center">
+		<p class="mb-8 text-center text-base-400">
 			We'd love to hear from you. Reach out to us for any inquiries or feedback.
 		</p>
 		<div class="grid grid-cols-1 gap-12 md:grid-cols-2">
 			<div>
-				<form class="space-y-5 " onsubmit={handleSubmit}>
+				<form class="space-y-5" onsubmit={handleSubmit}>
 					<div>
-						<InputField placeholder="Name"  bind:value={name} size="lg" />
+						<InputField placeholder="Name" name="name" type="text" bind:value={name} size="lg" />
 					</div>
 
 					<div>
-						<EmailField placeholder="Email Address" bind:value={email} size="lg" />
+						<EmailField
+							placeholder="Email Address"
+							name="email"
+							type="email"
+							bind:value={email}
+							size="lg"
+						/>
 					</div>
 
 					<!-- <div>
@@ -85,28 +87,35 @@
 					</div> -->
 
 					<div>
-						<Button label="Send Message" type="submit" appearance="border-primary" className="w-full" />
+						<Button
+							label="Send Message"
+							type="submit"
+							appearance="border-primary"
+							className="w-full"
+						/>
 					</div>
 				</form>
 			</div>
 			<div>
-				<div class="bg-base-800 h-full w-full rounded-lg p-6 md:p-8">
+				<div class="h-full w-full rounded-lg bg-base-800 p-6 md:p-8">
 					<h3 class="mb-4 text-lg font-bold text-white md:text-xl">Contact Information</h3>
-					<p class="text-base-400 mb-4">
+					<p class="mb-4 text-base-400">
 						Have questions? We're here to help. Reach out to us for any inquiries or feedback.
 					</p>
-					<p class="text-base-400 mb-4">
-						Email: <a href="mailto:support@hellocall.ai" class="text-primary-500"
-							>support@hellocall.ai</a
+					<p class="mb-4 text-base-400">
+						Email: <a
+							href="mailto:
+                  lampvoltoffice@gmail.com"
+							class="text-primary-500"
+						>
+							lampvoltoffice@gmail.com</a
 						>
 					</p>
-					<p class="text-base-400 mb-4">
-						Phone: <a href="tel:+91-8050770132" class="text-primary-500">+91-8050770132</a>
+					<p class="mb-4 text-base-400">
+						Phone: <a href="tel:+91 6370888479" class="text-primary-500">+91 6370888479</a>
 					</p>
 				</div>
 			</div>
 		</div>
 	</div>
 </div>
-
-
